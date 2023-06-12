0c983869a8b3c4157823675238a20c7dc7a7d8a2
Zinok <chevohnoe@mail.ru>
2023-06-12 12:01:28 +0300

    Функции для тренировки

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,3 @@
-/** Функция для проверки длины строки.
-* true, если строка меньше или равна указанной длине
-* false, если строка длиннее.
-*/
 const GET_STRING_LENGHT = (testString, stringLength) => {
   if (testString.length > stringLength) {
     return false;
@@ -11,11 +7,9 @@ const GET_STRING_LENGHT = (testString, stringLength) => {
 
 GET_STRING_LENGHT('хочется как можно меньше работать и больше веселиться', 35);
 
-/** Функция для проверки, является ли строка палиндромом.
-* true, если cтрока является палиндромом несмотря на разный регистр
-* false, если это не палиндром.
+/**
 * приводим к одному регистру, удалям пробеллы в строке
-* делаем все буквы эл массива, меняем положение и удаляем все запятые и пробелы самого массива
+* делаем все буквы элементами массива, меняем положение и удаляем все запятые и пробелы самого массива
 */
 const CHECK_PALINDROM = (palindrome) => {
   if (palindrome.toUpperCase().replaceAll(' ', '') === palindrome.toUpperCase().replaceAll(' ', '').split('').reverse().join('')) {
