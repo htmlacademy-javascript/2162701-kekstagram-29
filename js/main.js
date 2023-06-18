950ae0cba13e351e63704bf397366171ffe4cfe7
Zinok <chevohnoe@mail.ru>
2023-06-18 18:39:49 +0300

    генерация данных

--- js/main.js
+++ js/main.js
@@ -24,8 +24,45 @@ function getRandomInteger (min, max) {
   return Math.floor(result);
 }
 
-// Генерация случайного элемента массива
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+//генерация случайного элемента массива
+// eslint-disable-next-line no-undef
+const getRandomArrayElement = (elements,) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
+// Генерация порядкого номера
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+const generatePhotoId = createIdGenerator(); // id фото по порядку
+const generatePhoUrl = createIdGenerator(); // Url фото по порядку
+const generateComments = createIdGenerator(); // comments под фото по порядку
+
+const photoPostedByUser = () => {
+  const photoСomments = () => { //ф-ия по созданию массив объектов — список комментариев
+    const arrComments = []; // массив для хранения объектов
+    for (let i = 0; i < getRandomInteger(1, 30); i++) { //диапазон коментариев, к одной фото
+      arrComments.push({
+        id: generateComments(),
+        avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+        message: getRandomArrayElement(massages),
+        name: getRandomArrayElement(userName)
+      });
+    }
+    return arrComments;
+  };
+
+  return {
+    id: generatePhotoId(),
+    url: `photos/${generatePhoUrl()}.jpg`,
+    description: getRandomArrayElement(descriptionFoto),
+    likes: getRandomInteger(15, 200),
+    comments: photoСomments()
+  };
+};
 // eslint-disable-next-line no-console
-console.log(userName, massages, descriptionFoto, getRandomArrayElement());
+console.log(Array.from({length: 25}, photoPostedByUser));
