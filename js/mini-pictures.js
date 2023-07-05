7503bb9a1a903af993f3052e23cff8815e0e89ca
Zinok <chevohnoe@mail.ru>
2023-07-05 03:22:24 +0300

    не работает, когда модуль modal создан отдельно

--- js/mini-pictures.js
+++ js/mini-pictures.js
@@ -1,23 +1,30 @@
-const picturesContainerElement = document.querySelector('.pictures'); //ищем куда складывать фото
+import {openUserBigFoto} from './modal_big_pictures';
+
+const pictureContainerElement = document.querySelector('.pictures'); //ищем куда складывать фото
 const pictureTemplateElement = document.querySelector('#picture').content.querySelector('.picture'); //ищем шаблон
+const pictureListFragment = document.createDocumentFragment();
 
 /**
  * Функция по отрисовки миниатюр
  * @param {Object} drawThumbnails - массив объектов
  */
 const createThumbnail = (drawThumbnails) => {
-  const pictureListFragment = document.createDocumentFragment();
-  drawThumbnails.forEach((thumbnail) => { //перебираем эл массива отрисованных миниатюр
+  drawThumbnails.forEach(({url, description, likes, comments}) => { //перебираем эл массива отрисованных миниатюр
     const pictureElement = pictureTemplateElement.cloneNode(true); //клонирование элемента со всеми вложенностями
 
-    const [url, description, likes, comments] = [thumbnail.url, thumbnail.description, thumbnail.likes, thumbnail.comments];
+    //const {url, description, likes, comments} = thumbnail; //деструктуризация объекта
     pictureElement.querySelector('.picture__img').src = url; //добавляем фото
     pictureElement.querySelector('.picture__img').alt = description;
     pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments;
-    picturesContainerElement.appendChild(pictureElement); //вставляем на страницу
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureContainerElement.append(pictureElement); //вставляем на страницу
+
+    pictureElement.addEventListener('click', () => { //создаем замыкание
+      openUserBigFoto({url, description, likes, comments});
+    });
+
   });
-  picturesContainerElement.appendChild(pictureListFragment);
+  pictureContainerElement.append(pictureListFragment);
 };
 
 export {createThumbnail};
