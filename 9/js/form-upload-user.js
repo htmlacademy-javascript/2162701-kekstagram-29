472a2e82bc3658ca7aaf1b43622439fcd3a6a21f
Keks <keks@htmlacademy.ru>
2023-07-17 01:26:37 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/form-upload-user.js
@@ -0,0 +1,69 @@
+import { isEscapeKey } from './util.js';
+import { resetPristine, setValidaator, isInputFocus } from './form-validation.js';
+import { initSlider, hideSlider, resetEffect } from './slider.js';
+import { resetScale, initScale } from './scale.js';
+
+const uploadForm = document.querySelector('.img-upload__form'); //форма загрузки
+const uploadOverlay = uploadForm.querySelector('.img-upload__overlay'); //подложка
+const uploadInput = uploadForm.querySelector('.img-upload__input'); //контрол загрузки файла
+const uploadCancel = uploadForm.querySelector('.img-upload__cancel'); //кнопка закрыть
+
+/**
+ * функция для закрытия подложки с помощью клавиатуры, за исключением, когда поле ввода в фокусе
+ * @param {object} evt объект события
+ */
+function onDocumentKeydown (evt) {
+  if (isEscapeKey(evt) && !(isInputFocus())) {
+    evt.preventDefault();
+    closeUserOverlay();
+  }
+}
+
+/**
+ * функция для открытия подложки
+ */
+const openUserOverlay = () => {
+  uploadOverlay.classList.remove('hidden'); // 1. Показать подложку
+  document.body.classList.add('modal-open');//2. отключаем скрол под подложкой
+  document.addEventListener('keydown', onDocumentKeydown); // 3. Добавить обработчики для закрытия на клавишу
+  hideSlider(); //скрывается слайдер при первоночальном показе
+};
+
+/**
+ * функция для закрытия подложки
+ */
+function closeUserOverlay () {
+  uploadForm.reset(); // восстанавливает стандартные значения
+  resetScale(); //сброс эффектов маштаба
+  resetEffect(); //сброс эффектов слайдера
+  resetPristine(); //сброс ошибок pristine
+  uploadOverlay.classList.add('hidden'); // 1. Скрыть подложку
+  document.body.classList.remove('modal-open');// 2. включить скрол
+  document.removeEventListener('keydown', onDocumentKeydown); //3. удалить обработчик событий при нажатии на клавишу
+}
+
+//при клике на кнопку закрыть
+uploadCancel.addEventListener('click', () => {
+  closeUserOverlay();
+});
+
+//открытие модалки при событии change
+uploadInput.addEventListener('change', () => {
+  openUserOverlay();
+});
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+});
+
+/**
+ * инициализация формы загрузки
+ */
+const initUploadForm = () => {
+  //uploadForm.addEventListener('submit', validationCheck); //проверка на валидацию
+  initSlider(); //бегунок слайдера
+  initScale(); // маштаб
+  setValidaator(); //проверка на валидацию
+};
+
+export { initUploadForm };
---
+++ 9/js/util.js
@@ -0,0 +1,46 @@
+//Функции для моков
+/**
+ * Функция для создания случайного числа в диапозоне от min до max
+ * @param {number} min - нижняя граница диапозона
+ * @param {number} max - верхняя граница диапозона
+ * @return {number} - возвращает случайное число в диапозоне от min до max
+ */
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+/**
+ * Функция для генерации случайного элемента массива
+ * @param {array} elements - массив данных для генерации
+ * @return {string} - возвращает элемент массива elements
+ */
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+/**
+ * Функция для генерации порядкого номера
+ * @return {number} - возвращает порядковый номер
+ */
+const getIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+const bigFotoElement = document.querySelector('.big-picture'); //модальное окно
+//функция для чистоты кода
+const isEscapeKey = (evt) => evt.key === 'Escape';
+const isModalTarget = (evt) => evt.target === bigFotoElement;
+
+//функции для моков
+export {getRandomInteger};
+export {getRandomArrayElement};
+export {getIdGenerator};
+//функция для чистоты кода
+export { isEscapeKey, isModalTarget };
+
